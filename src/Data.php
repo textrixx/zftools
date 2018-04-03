de4b1ffa45839953cb9f61b38d5c5fa293c6004a
Marco Mertens <admin@rhombus-rollen.de>
2018-04-03 16:27:07 +0200

    Changes license to Apache 2.0

--- src/Data.php
+++ src/Data.php
@@ -1,25 +1,27 @@
 <?php
 /**
- * Textrixx Data
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file license.txt.
- * It is also available through the world-wide-web at this URL:
- * https://www.textrixx.com/license/mit.txt
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to info@textrixx.com so we can send you a copy immediately.
- * 
- * @category	Zftools
- * @package		helper
- * @author		Marco Mertens
- * @copyright	Copyright (c) 2018 textrixx - netzloesungen & kommunikation
- * @license		http://www.margott.de/license/mit.txt MIT License
- * @version		1.0.0
- * 
- */
+* Copyright 2018 Marco Mertens, textrixx - netzloesungen & kommunikation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* @category	Zftools
+* @package		helper
+* @author		Marco Mertens
+* @copyright	Copyright (c) 2018 textrixx - netzloesungen & kommunikation
+* @license		http://www.margott.de/license/mit.txt MIT License
+* @version		1.0.0
+* 
+*/
 namespace Textrixx\Zftools;
 
 use Zftools\Zftools;
